0012aa98497d3219c3883adf4d9599332360eda3
haurog <36535774+haurog@users.noreply.github.com>
2022-09-06 21:18:03 +0200

    initial commit of new page

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# discarbon-website-main
-Main website for the disCarbon tools
+# discarbon-main-website
+
+The website can be accessed under `https://www.discarbon.earth/`. This directly reflects the main branch. This is in development so expect things to change and break.
+
+## How to develop
+
+`npm i` to install all components
+
+Should install tailwind CSS and Daisy UI for the UI components.
+
+Open a console and start the tailwind css watcher which compiles the css automatically depending on the used components:
+
+`npx tailwindcss -i ./css/input.css -o ./css/output.css --watch`
+
+Run site for local development with HTTPS via [https-localhost](https://github.com/daquinoaldo/https-localhost):
+```sh
+npm i -g --only=prod https-localhost
+sudo serve .
+```
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["*.{html,js}"],
+  purge: ["*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'RollingHills': "url('/images/RollingHills.svg')",
+      },
+      minHeight: (theme) => ({
+        ...theme('spacing'),
+      }),
+      fontFamily: {
+        sans: [
+          'Helvetica', 'FreeSans',
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    }
+  },
+  plugins: [require("daisyui")],
+
+    // daisyUI config (optional)
+    daisyui: {
+      styled: true,
+      themes: true,
+      base: true,
+      utils: true,
+      logs: true,
+      rtl: false,
+      prefix: "",
+      darkTheme: "forest",
+      themes:["emerald"],
+    },
+}
